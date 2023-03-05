2535801367cdcc2fce814d94dc0787067f0c3ac2
= <=>
2023-03-05 22:52:30 +0300

    laba 1 запись рандомных данных по пути, который указывает user

--- Laba1.cpp
+++ Laba1.cpp
@@ -1,7 +1,10 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
 #include <Windows.h>
 #include <Wincrypt.h>
 #include <stdlib.h>
+#include <fstream>
 
 int main()
 {
@@ -11,21 +14,39 @@ int main()
     int random_number = rand() % 100;
     byte* random_data_generate = (byte*)malloc(random_number);
     HCRYPTPROV hprov;
-    /*
-        Используется CRYPT_VERIFYCONTEXT, так как с 0 не работает
-    */
-    CryptAcquireContext(&hprov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT); 
+    char * filePath;
+    filePath = (char *)malloc(100);
+
+    printf("Введите путь к файлу: ");
+    scanf("%s", filePath);
+
+    std::ofstream out;         
+    out.open(filePath); 
+
+    CryptAcquireContext(&hprov, NULL, NULL, 1, CRYPT_VERIFYCONTEXT); 
     CryptGenRandom(hprov, random_number, random_data_generate);
-
-    printf("Произвольная строка длины: %d\n", random_number);
-
-    for (int i = 0; i < random_number; i++) {
-        if (i != (random_number - 1))
-            printf("%d, ", random_data_generate[i]);
-        else
-            printf("%d\n", random_data_generate[i]);
+  
+    if (out.is_open()) {
+        out << "Random data with length " << random_number << "\n";
+        out << "As string\n";
+        for (int i = 0; i < random_number; i++) {
+                out << random_data_generate[i];
+        }
+        out << "\nAs integer\n";
+        for (int i = 0; i < random_number; i++) {
+            if (i != (random_number - 1))
+                out << (int)random_data_generate[i] << ", ";
+            else
+                out << (int)random_data_generate[i] << ".";
+        }
+    }
+    else {
+        printf("Ошибка при записи файла.");
     }
 
     CryptReleaseContext(hprov, 0);
     free(random_data_generate);
+    free(filePath);
 }
+
+
