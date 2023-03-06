ed4ef732f80517735309e1ff3e98755e30514d75
= <=>
2023-03-06 22:06:32 +0300

    лаба 1, создание контекста с разными криптопровайдерами

--- Laba1.cpp
+++ Laba1.cpp
@@ -5,36 +5,67 @@
 #include <Wincrypt.h>
 #include <stdlib.h>
 #include <fstream>
+/*
+    shell command "certutil -csplist"
+*/
+/*
+    file_path "D:\abs.txt"
+*/
+typedef struct {
+    const char* provFullName;
+    const wchar_t* provPsevdonim;
+    int provType;
+} Provider;
+
+void menuProviders(int* input, Provider* provider, Provider* providers, int provNumber);
+
+void inputArr(int* length);
 
 int main()
 {
+    Provider providers[10] = { {MS_DEF_PROV_A, MS_DEF_PROV, PROV_RSA_FULL},
+                                {MS_DEF_DSS_DH_PROV_A, MS_DEF_DSS_DH_PROV, PROV_DSS_DH},
+                                {MS_DEF_DSS_PROV_A, MS_DEF_DSS_PROV, PROV_DSS},
+                                {MS_SCARD_PROV_A, MS_SCARD_PROV, PROV_RSA_FULL},
+                                {MS_DEF_DH_SCHANNEL_PROV_A, MS_DEF_DH_SCHANNEL_PROV, PROV_DH_SCHANNEL},
+                                {MS_ENHANCED_PROV_A, MS_ENHANCED_PROV, PROV_RSA_FULL},
+                                {MS_ENH_DSS_DH_PROV_A, MS_ENH_DSS_DH_PROV, PROV_DSS_DH},
+                                {MS_ENH_RSA_AES_PROV_A, MS_ENH_RSA_AES_PROV, PROV_RSA_AES},
+                                {MS_DEF_RSA_SCHANNEL_PROV_A, MS_DEF_RSA_SCHANNEL_PROV, PROV_RSA_SCHANNEL},
+                                {MS_STRONG_PROV_A, MS_STRONG_PROV, PROV_RSA_FULL} };
     setlocale(LC_ALL, "rus");
-    srand(time(NULL));
+    Provider prov = {};
+    int input = 0;
 
-    int random_number = rand() % 100;
-    byte* random_data_generate = (byte*)malloc(random_number);
+    menuProviders(&input, &prov, providers, 10);
+
+    int dataLength = 0;
+    inputArr(&dataLength);
+    byte* random_data_generate = (byte*)malloc(dataLength);
+
     HCRYPTPROV hprov;
-    char * filePath;
-    filePath = (char *)malloc(100);
 
-    printf("Введите путь к файлу: ");
+    char* filePath;
+    filePath = (char*)malloc(100);
+
+    printf("\nВведите путь к файлу: ");
     scanf("%s", filePath);
 
-    std::ofstream out;         
-    out.open(filePath); 
+    std::ofstream out;
+    out.open(filePath);
 
-    CryptAcquireContext(&hprov, NULL, NULL, 1, CRYPT_VERIFYCONTEXT); 
-    CryptGenRandom(hprov, random_number, random_data_generate);
-  
+    CryptAcquireContext(&hprov, NULL, prov.provPsevdonim, prov.provType, CRYPT_VERIFYCONTEXT);
+    CryptGenRandom(hprov, dataLength, random_data_generate);
+
     if (out.is_open()) {
-        out << "Random data with length " << random_number << "\n";
+        out << "Random data with length " << dataLength << "\n";
         out << "As string\n";
-        for (int i = 0; i < random_number; i++) {
-                out << random_data_generate[i];
+        for (int i = 0; i < dataLength; i++) {
+            out << random_data_generate[i];
         }
         out << "\nAs integer\n";
-        for (int i = 0; i < random_number; i++) {
-            if (i != (random_number - 1))
+        for (int i = 0; i < dataLength; i++) {
+            if (i != (dataLength - 1))
                 out << (int)random_data_generate[i] << ", ";
             else
                 out << (int)random_data_generate[i] << ".";
@@ -47,6 +78,30 @@ int main()
     CryptReleaseContext(hprov, 0);
     free(random_data_generate);
     free(filePath);
+
+    system("pause");
 }
 
+void menuProviders(int* input, Provider* provider, Provider* providers, int provNumber) {
+    printf("Выберете криптопровайдера из списка: \n");
+    for (int i = 0; i < provNumber; i++) {
+        printf("\t%d). %s\n", i + 1, providers[i].provFullName);
+    }
+    scanf("%d", input);
+    if ((*input < 1) || (*input > provNumber)) {
+        printf("Данные некорректны! Повторите ввод.\n");
+        menuProviders(input, provider, providers, provNumber);
+    }
+    else {
+        *provider = providers[*input - 1];
+    }
+}
 
+void inputArr(int* length) {
+    printf("Введите длину произвольного массива данных: ");
+    scanf("%d", length);
+    if (!(length > 0)) {
+        printf("Данные некорректны! Повторите ввод.\n");
+        inputArr(length);
+    }
+}
